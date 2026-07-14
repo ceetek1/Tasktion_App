6dad28b22257bfaa5e91944c1790523355e70a0c
ceetek1 <charles.chukwurah@pau.edu.ng>
2026-07-14 17:30:24 +0100

    feat: add SQLAlchemy engine,session, and Base ORM

---
+++ backend/app/core/database.py
@@ -0,0 +1,18 @@
+from app.core.config import settings 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase,sessionmaker
+DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL)
+
+class Base(DeclarativeBase):
+    pass
+
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
