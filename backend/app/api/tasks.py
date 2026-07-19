6da3990753e9f8f5387064e271e6adddeda6a7cd
ceetek1 <charles.chukwurah@pau.edu.ng>
2026-07-19 15:44:05 +0100

    feat: add POST /tasks endpoint

--- backend/app/api/tasks.py
+++ backend/app/api/tasks.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.models.task import Task
 from app.schemas.task import TaskRead
+from app.schemas.task import TaskCreate
+from app.models.user import User
+
 
 router = APIRouter()
 
@@ -16,3 +19,11 @@ def get_task(task_id: int, db: Session = Depends(get_db)):
     if not task:
         raise HTTPException(status_code=404, detail='Task not found')
     return task
+
+@router.post("/tasks",response_model= TaskRead,status_code=201)
+def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+    new_task = Task(title= task.title, description=task.description, due_date=task.due_date, user_id=1)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
