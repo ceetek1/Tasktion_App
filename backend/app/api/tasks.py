2b9393bc2f4cff8af07ea03b58cf1d22ce8c6051
ceetek1 <charles.chukwurah@pau.edu.ng>
2026-07-20 13:54:04 +0100

    feat: add PATCH/tasks/{id}endpoint with partial updates

--- backend/app/api/tasks.py
+++ backend/app/api/tasks.py
@@ -5,6 +5,7 @@ from app.models.task import Task
 from app.schemas.task import TaskRead
 from app.schemas.task import TaskCreate
 from app.models.user import User
+from app.schemas.task import TaskUpdate
 
 
 router = APIRouter()
@@ -27,3 +28,16 @@ def create_task(task: TaskCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_task)
     return new_task
+
+@router.patch("/tasks/{task_id}",response_model= TaskRead)
+def update_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db)):
+    existing_task = db.query(Task).filter(Task.id == task_id).first()
+    if not existing_task:
+        raise HTTPException(status_code=404, detail='Task not found')
+    task = task.model_dump(exclude_unset=True)
+    
+    for key , value in task.items():
+        setattr(existing_task, key, value)
+    db.commit()
+    db.refresh(existing_task)
+    return existing_task
--- backend/app/schemas/task.py
+++ backend/app/schemas/task.py
@@ -20,3 +20,11 @@ class TaskRead(TaskBase):
 
 
     model_config = ConfigDict(from_attributes= True)
+
+
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    due_date: datetime | None = None
+    status: str | None = None
