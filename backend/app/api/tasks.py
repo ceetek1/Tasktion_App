2dd39f741b30c067c7c4077a64d08573349039a1
ceetek1 <charles.chukwurah@pau.edu.ng>
2026-07-26 18:28:21 +0100

    fix: enforce task ownersip on all task endpoints

--- backend/app/api/tasks.py
+++ backend/app/api/tasks.py
@@ -6,32 +6,33 @@ from app.schemas.task import TaskRead
 from app.schemas.task import TaskCreate
 from app.models.user import User
 from app.schemas.task import TaskUpdate
+from app.api.auth import get_current_user
 
 
 router = APIRouter()
 
 @router.get("/tasks",response_model= list[TaskRead])
-def get_tasks(db: Session = Depends(get_db)):
-    return db.query(Task).all()
+def get_tasks(db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+    return db.query(Task).filter(Task.user_id == current_user.id).all()
 
 @router.get("/tasks/{task_id}",response_model= TaskRead)
-def get_task(task_id: int, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == task_id).first()
+def get_task(task_id: int, db: Session = Depends(get_db), current_user: User =Depends(get_current_user)):
+    task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
     if not task:
         raise HTTPException(status_code=404, detail='Task not found')
     return task
 
 @router.post("/tasks",response_model= TaskRead,status_code=201)
-def create_task(task: TaskCreate, db: Session = Depends(get_db)):
-    new_task = Task(title= task.title, description=task.description, due_date=task.due_date, user_id=1)
+def create_task(task: TaskCreate, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+    new_task = Task(title= task.title, description=task.description, due_date=task.due_date,user_id=current_user.id)
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
     return new_task
 
 @router.patch("/tasks/{task_id}",response_model= TaskRead)
-def update_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db)):
-    existing_task = db.query(Task).filter(Task.id == task_id).first()
+def update_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    existing_task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
     if not existing_task:
         raise HTTPException(status_code=404, detail='Task not found')
     task = task.model_dump(exclude_unset=True)
@@ -44,8 +45,8 @@ def update_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db)):
 
 
 @router.delete("/tasks/{task_id}",status_code=204)
-def delete_task(task_id: int, db:Session = Depends(get_db)):
-    existing_task = db.query(Task).filter(Task.id == task_id).first()
+def delete_task(task_id: int, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    existing_task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
     if not existing_task:
         raise HTTPException(status_code=404, detail='Task not found')
     db.delete(existing_task)
